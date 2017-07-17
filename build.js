3529baacd55d63b5f89a459d3116d0e334fc772a
Thiago Santos <thia.mdossantos@gmail.com>
2017-07-17 13:35:17 -0300

    :art: Improve async of build script

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
-const {readFileSync} = require('fs')
+const {promisify} = require('util')
+const fs = require('fs')
 const {rollup} = require('rollup')
 const babel = require('rollup-plugin-babel')
 const commonjs = require('rollup-plugin-commonjs')
@@ -6,7 +7,8 @@ const nodeResolve = require('rollup-plugin-node-resolve')
 const filesize = require('rollup-plugin-filesize')
 const uglify = require('rollup-plugin-uglify')
 
-const entry = 'lib/index.js'
+const readFile = promisify(fs.readFile)
+
 const targets = {
   cjs: 'dist/vanilla-commons.js',
   es: 'dist/vanilla-commons.es.js',
@@ -14,45 +16,49 @@ const targets = {
   min: 'dist/vanilla-commons.umd.min.js'
 }
 
-const pkg = JSON.parse(readFileSync('./package.json'))
-const dependencies = Object.keys(pkg.dependencies || {})
+async function getDependencies() {
+  const pkg = JSON.parse(await readFile('./package.json'))
+  return Object.keys(pkg.dependencies || {})
+}
 
-const defaultPlugins = [
-  nodeResolve(),
-  commonjs({
-    include: 'node_modules/**'
-  }),
-  babel({
-    babelrc: false,
-    presets: [
-      [
-        'env',
-        {
-          modules: false
-        }
+async function generateBundle(format) {
+  const defaultPlugins = [
+    nodeResolve(),
+    commonjs({
+      include: 'node_modules/**'
+    }),
+    babel({
+      babelrc: false,
+      presets: [
+        [
+          'env',
+          {
+            modules: false
+          }
+        ],
+        'stage-0'
       ],
-      'stage-0'
-    ],
-    plugins: ['external-helpers']
-  }),
-  filesize()
-]
+      plugins: ['external-helpers']
+    }),
+    filesize()
+  ]
 
-async function build(format) {
   const plugins =
     format === 'min' ? defaultPlugins.concat(uglify()) : defaultPlugins
 
   const basicConfig = {
-    entry,
+    entry: 'lib/index.js',
     plugins
   }
 
   const customConfig = ['cjs', 'es'].includes(format) ?
-    {external: dependencies} :
+    {external: await getDependencies()} :
     {}
 
-  const bundle = await rollup(Object.assign(basicConfig, customConfig))
+  return  rollup(Object.assign(basicConfig, customConfig))
+}
 
+function writeBundle(bundle, format) {
   return bundle.write({
     dest: targets[format],
     format: format === 'min' ? 'umd' : format,
@@ -60,9 +66,32 @@ async function build(format) {
   })
 }
 
+async function buildGeneral() {
+  const bundle = await generateBundle('es')
+
+  return Promise.all([
+    await writeBundle(bundle, 'es'),
+    await writeBundle(bundle, 'cjs')
+  ])
+}
+
+async function buildUmd() {
+  const bundle = await generateBundle('umd')
+  return writeBundle(bundle, 'umd')
+}
+
+async function buildMin() {
+  const bundle = await generateBundle('min')
+  return writeBundle(bundle, 'min')
+}
+
 async function run() {
   try {
-    await Promise.all(Object.keys(targets).map(build))
+    Promise.all([
+      buildGeneral(),
+      buildUmd(),
+      buildMin()
+    ])
   } catch (err) {
     console.error(err)
   }
